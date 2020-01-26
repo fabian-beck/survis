57a0677b6e53867185318cf3b72e6a85f3d45b59
Fabian Beck <fabian@fbeck.com>
2020-01-26 10:44:48 +0100

    allowing semicolon as an alternative splitter for keyword strings

--- src/js/app/util.js
+++ src/js/app/util.js
@@ -166,7 +166,8 @@ define(['jquery', 'jquery.tooltipster'], function ($, tooltipster) {
                 switch (field) {
                     case 'keywords':
                         var tags = [];
-                        $.each(fieldString.split(','), function (i, tag) {
+                        const splitter = fieldString.indexOf(',') < 0?';':',';
+                        $.each(fieldString.split(splitter), function (i, tag) {
                             tag = $.trim(tag.split("\\").join("")).toLowerCase();
                             if ($.inArray(tag, tags) == -1) {
                                 tags.push(tag);
